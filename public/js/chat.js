1cbbc2d6a9af81c7142b9444271dafc386dba342
Jerry Yang Jin <jerry.yang.jin@gmail.com>
2014-07-15 03:27:19 +0000

    nick name

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,11 +1,24 @@
 $(function(){
     var socket = io();
-      $('form').submit(function(){
+
+    socket.on('chat message', function(msg){
+      $('#messages').append($('<li>').text(msg));
+    });
+    
+    $('#mf').submit(function(){
+      if($('#nick').val()){
         socket.emit('chat message', $('#m').val());
         $('#m').val('');
-        return false;
-      });
-      socket.on('chat message', function(msg){
-        $('#messages').append($('<li>').text(msg));
-      });
+      }
+      return false;
+    });
+    
+    $('#ff').submit(function(){
+      $('#nav-name').text(($('#nick').val()?$('#nick').val():'nick name not set'));
+      if($('#nick').val()){
+        socket.emit('register', $('#nick').val());
+      }
+      return false;
+    });    
+
 })
