5c6448821b35acecd3037d82edc3b3251711efe6
Jerry Yang Jin <jerry.yang.jin@gmail.com>
2014-07-15 23:02:20 +0800

    broadcast

--- public/js/chat.js
+++ public/js/chat.js
@@ -4,21 +4,29 @@ $(function(){
     socket.on('chat message', function(msg){
       $('#messages').append($('<li>').text(msg));
     });
-    
+
     $('#mf').submit(function(){
       if($('#nick').val()){
-        socket.emit('chat message', $('#m').val());
+        var data = {
+          name: $('#nick').val(),
+          msg: $('#m').val()
+        }
+        socket.emit('chat message', data);
         $('#m').val('');
+        $('#messages').append($('<li>').text(data.msg));
       }
       return false;
     });
-    
+
     $('#ff').submit(function(){
       $('#nav-name').text(($('#nick').val()?$('#nick').val():'nick name not set'));
       if($('#nick').val()){
-        socket.emit('register', $('#nick').val());
+        var data = {
+          name: $('#nick').val(),
+        }
+        socket.emit('register', data);
       }
       return false;
-    });    
+    });
 
 })
